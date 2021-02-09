d1a3f0d8096885f7c5ddf96a0afae449be35e903
anitas-giraldo <anita.giraldo@marineecology.io>
2021-02-09 02:07:04 +0000

    commit n3

---
+++ MBdesignGB-n3/outputs/newdesign/2.1.2.Set_transect_design.R
@@ -0,0 +1,76 @@
+#### Set up transect design ####
+incprobm <- as.matrix(inclProbs) # need to rearrange the order of this for better plotting
+n.x <- nrow( incprobm)
+n.y <- ncol( incprobm)
+image.plot( x=1:n.x, y=1:n.y, z=incprobm, main="Geographe Bay bathymetry (m)", asp=1)
+
+# transpose 
+incprobm2 <- t(incprobm)
+n.x <- nrow( incprobm2)
+n.y <- ncol( incprobm2)
+image.plot( x=1:n.x, y=1:n.y, z=incprobm2, main="Geographe Bay bathymetry (m)", asp=1)
+
+#Fix up ordering issue
+incprobm2 <- incprobm2[, rev ( 1 : ncol (incprobm2))]
+n.x <- nrow( incprobm2)
+n.y <- ncol( incprobm2)
+image.plot( x=1:n.x, y=1:n.y, z=incprobm2, main="Geographe Bay bathymetry (m)", asp=1)
+
+cellStats(inclProbs, sum)
+
+#format for MBHdesign functions
+incprobdf <- expand.grid( x=1:n.x, y=1:n.y)
+incprobdf$IncProbs <- as.vector( incprobm2)
+head(incprobdf)
+
+
+#gb.control is a list that contains transect details
+gb.control <- list(
+  #the type of transect
+  transect.pattern="line",
+  #the length of transect
+  line.length=250,
+  #the number of points that define the transect
+  transect.nPts=5,
+  #the number of putative directions that a transect can take
+  nRotate=11, 
+  mc.cores = 25,
+  transAdjust = FALSE,
+  edge.max.iter = 5,
+  nSampsToConsider = 2500000
+)
+
+## Create constrains, but from previous trials, there are no constrains in GB ####
+transAdjust = FALSE### to avoid transects that descend or ascend in bathymetry we use ----
+gb.constrains <- findDescendingTrans(
+  potential.sites = pot.sites[,c("Easting","Northing")], bathy = pot.sites$depth,
+  in.area = rep( TRUE, nrow( pot.sites)), control = gb.control
+)
+
+print( dim( gb.constrains)) # [1] 36395    11
+
+#The contents describe how the transect lays over the landscape
+#So, there are 15592 putative transects that ascend and descend
+# (and can't be used in the sample)
+table( as.vector(gb.constrains)) #   230868           164099              426             4952
+
+#convert to TRUE/FALSE
+#Note that the final possible transect type ('descendAndNA') is
+# not present in these data
+#If present, we would have to decide to sample these or not
+
+gb.constraints.bool <- matrix( FALSE, nrow=nrow( gb.constrains),
+                               ncol=ncol( gb.constrains))
+gb.constraints.bool[gb.constrains %in% c("descend")] <- TRUE
+
+#Let's get a visual to see what has just been done.### this is not working now ----
+#tmpMat <- matrix( apply( gb.constraints.bool, 1, sum), nrow=nrow( gb.constrains), ncol=ncol( gb.constrains))
+#tmpMat <- matrix( apply( gb.constraints.bool, 1, sum), nrow=nrow( pot.sites), ncol=ncol( pot.sites))
+tmpMat <- matrix( apply( gb.constraints.bool, 1, sum), nrow=n.y, ncol=n.x)
+#tmpMat2 <- t(tmpMat)
+
+image.plot( y=1:n.x, x=1:n.y, z=tmpMat,
+            main="Number of Transects",
+            sub="Transects centered at cell (max 11)", asp=1)
+
+
---
+++ MBdesignGB-n3/outputs/newdesign/2.2.IncProbs.R
@@ -0,0 +1,151 @@
+### Create inclusion probabilities ####
+
+library( rgdal)
+library( sp)
+library( raster)
+
+#########################
+#read in data
+
+setwd("~/MBHdesignGB/outputs/")
+
+GBDat <- readRDS( "GBData_forDesign1.RDS")
+gb_rasters <- readRDS("GBRasters_forDesign1.RDS")
+zones <- readRDS( "GBZones_forDesign1.RDS")
+
+####################################
+####  Straw man for numbers of samples in each region
+####################################
+
+straw.nums <- c( 6, 6, 3, 3)  #numbers of drops
+straw.props <- straw.nums / sum( straw.nums)
+names( straw.nums) <- names( straw.props) <- c( "MUZ", "SPZ", "HPZ", "NPZ")
+saveRDS( straw.nums, file="StrawmanNumbers_Zones.RDS")
+
+###################################
+####  Hand-picking Bathy cut points
+####  And their numbers of drops
+###################################
+
+#Bathy.quant <- c(0,0.8,0.9,0.925,0.95,0.975,1)
+Bathy.quant <- c(0,0.5,0.75,0.9,1)
+Bathy.cuts <- quantile( gb_rasters$bathy, Bathy.quant)#c( -Inf,0.02,0.04,0.08,0.16,Inf)
+#trying to make it so there is no hand-picking (except for the hand-picked function)
+tmp <- cumsum( Bathy.quant)
+Bathy.targetNums <- rep( floor( 18/8), 4)#floor( ( tmp / sum( tmp))[-1] * 200)#rep( 40, 5)#c( 20,20,30,65,65)
+Bathy.targetNums <- c(6,6,3,3)
+Bathy.targetProps <-  Bathy.targetNums / sum( Bathy.targetNums)
+
+########################
+#depth limiting and some elementary cleaning
+
+#minDepthInAMP <- max( NingalooDat$BATHY, na.rm=TRUE)
+
+#NingalooDat[ is.na( NingalooDat$BATHY) | NingalooDat$BATHY < -195 & NingalooDat$BATHY > minDepthInAMP, c("BATHY","TPI_GF")] <- NA
+
+GBDat[ !is.na( GBDat$BATHY) & GBDat$BATHY < -50 , "BATHY"] <- NA
+GBDat[ !is.na( GBDat$BATHY) & GBDat$BATHY > 0 , "BATHY"] <- NA
+
+setwd("~/MBHdesignGB/outputs/newdesign/")
+pdf( "Bathy_distribution_zones.pdf")
+{
+  
+  par( mfrow=c(2,2))
+  for( ii in c("MUZ", "SPZ", "HPZ", "NPZ"))
+    hist( GBDat[GBDat[,ii]==1,"BATHY"], nclass=50, main=ii)
+  
+  par( mfrow=c(1,1))
+  kount <- 1
+  plot( ecdf( GBDat[GBDat[,"MUZ"]==1,"BATHY"]), col=2, main="Bathy in Zones", xlim=c(-0.4,0.4))
+  for( ii in c("MUZ", "SPZ", "HPZ", "NPZ")){
+    plot( ecdf( GBDat[GBDat[,ii]==1,"BATHY"]), col=kount, add=TRUE)
+    kount <- kount + 1
+  }
+  legend( "bottomright", legend=c("MUZ", "SPZ", "HPZ", "NPZ"), lty=1, 
+          col=c(1:4), lwd=c(1,1,1,1), bty='n')
+  #so surprisingly, the southern control zone has more flat area than the IUCN2 area
+  #there may be some issues with the north control having quite a few more 'bumps' than the southern control and IUCN2 area.  Bias.
+  dev.off()
+}
+#########################
+#proportion of potential sites in each zone
+
+GBDat_small <- GBDat[!is.na( GBDat$BATHY),]
+tmp <- colSums( GBDat_small[,c("MUZ", "SPZ", "HPZ", "NPZ")], na.rm=TRUE) 
+tmp[2] <- tmp[2] - tmp[1] # so similar amount of sites in SPZ and MUZ
+props <- tmp / nrow( GBDat_small)
+props <- props / sum( props) # 1 UP TO HERE
+
+###################################
+####  TPI to get cut points
+###################################
+
+catB <- cut( gb_rasters$bathy, breaks=Bathy.cuts, na.rm=TRUE)
+
+plot( zones$MUZ); plot( catB, add=TRUE); plot( zones$MUZ, add=TRUE)
+
+writeRaster( catB, file='Bathy_cuts.tif', overwrite=TRUE)
+
+plot(catB)
+
+
+pdf( "~/MBHdesignGB/outputs/newdesign/InclusionProbs.pdf", height=7, width=8)
+plot( catB, main="Inclusion Probabilities for Zones Design") # -46.900249 -30.000000 -23.105238 -17.000000  -8.540641 
+plot( gb, add=TRUE)
+dev.off()
+
+##################################
+####  Within each zone (incl probs)
+####  Weight according to straw.props
+##################################
+
+
+
+inclProbs <- catB
+for( zz in c( "NPZ", "HPZ", "SPZ", "MUZ")){
+  print( zz)
+  #if( zz == "MUZ")
+    #zoneID <- extract( x=catB, y=zones$MUZ, cellnumbers=TRUE)
+    #zoneID <- extract( x=catB, y=zones$MUZ-zones$NPZ, cellnumbers=TRUE)
+  #else
+    zoneID <- extract( x=catB, y=zones[[zz]], cellnumbers=TRUE)
+  propsOfTPI <- table( catB@data@values[zoneID[[1]][,"cell"]])
+  propsOfTPI <- propsOfTPI / sum( propsOfTPI)
+  if(length(propsOfTPI) == 4)
+  tmp <- Bathy.targetProps / propsOfTPI #the desired inclusion probs (unstandardised)
+  else
+    tmp <- Bathy.targetProps2 / propsOfTPI #the desired inclusion probs (unstandardised)
+  for( ii in 1:length( propsOfTPI)){
+    inclProbs[zoneID[[1]][,"cell"]][zoneID[[1]][,"value"]==ii] <- tmp[ii]
+  }
+  inclProbs[zoneID[[1]][,"cell"]][is.na( inclProbs[zoneID[[1]][,"cell"]])] <- 0
+  inclProbs[zoneID[[1]][,"cell"]] <- inclProbs[zoneID[[1]][,"cell"]] / sum( inclProbs[zoneID[[1]][,"cell"]])
+}
+
+inclProbs@data@values[inclProbs@data@values %in% c(0,1,2,3,4)] <- NA  #cheats way to crop
+plot( inclProbs)
+
+#standardising so that the zone totals are correct according to straw.props | straw.nums
+MUZzone <- extract( x=catB, y=zones$MUZ, cellnumbers=TRUE)
+SPZzone <- extract( x=catB, y=zones$SPZ, cellnumbers=TRUE)
+HPZZone <- extract( x=catB, y=zones$HPZ, cellnumbers=TRUE)
+NPZZone <- extract( x=catB, y=zones$NPZ, cellnumbers=TRUE)
+
+inclProbs@data@values[MUZzone[[1]][,'cell']] <- inclProbs@data@values[MUZzone[[1]][,'cell']] * straw.props["MUZ"]
+inclProbs@data@values[SPZzone[[1]][,'cell']] <- inclProbs@data@values[SPZzone[[1]][,'cell']] * straw.props["SPZ"]
+inclProbs@data@values[HPZZone[[1]][,'cell']] <- inclProbs@data@values[HPZZone[[1]][,'cell']] * straw.props["HPZ"]
+inclProbs@data@values[NPZZone[[1]][,'cell']] <- inclProbs@data@values[NPZZone[[1]][,'cell']] * straw.props["NPZ"]
+
+plot(inclProbs)
+
+writeRaster( inclProbs, file='inclProbs_clus_design2.tif', overwrite=TRUE)
+
+pdf( "~/MBHdesignGB/outputs/newdesign/inclProbs_clus_design2.pdf", height=7, width=8)
+plot( inclProbs, main="Clustered transects design 250m (n=18)")
+plot( gb, add=TRUE)
+dev.off()
+
+rm( list=lso()$OTHERS)
+rm( AMPzone, catTPI, ii, IUCNzone, kount, minDepthInAMP, NingalooDat_small, NthZone, props, propsOfTPI, SthZone, 
+    tmp, zoneID, zz, TPI.cuts, TPI.quant, TPI.targetNums, TPI.targetProps)
+
